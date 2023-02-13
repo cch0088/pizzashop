19bf2e871375172702dc476cca6f000546c4d732
Constantine Cherkashyn <cons1@protonmail.ch>
2023-02-13 12:21:04 -0500

    Fix options

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,11 +1,16 @@
-
 import React from 'react';
-import {useState} from "react";
 
 function Options() {
 
   return (
     <div>
+    <div>Select Toppings</div>
+    <div className="options">
+      <div><input type="checkbox" />Pepperoni</div>
+      <div><input type="checkbox" />Olives</div>
+      <div><input type="checkbox" />Meatball</div>
+      <div><input type="checkbox" />Mushroom</div>
+    </div>
     </div>
   )
 }
