64da96f398324fbb2bcabe6375e44a597a343f12
Constantine Cherkashyn <cons1@protonmail.ch>
2023-02-13 14:22:35 -0500

    Console output of selected item

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,12 +1,15 @@
 import React from "react";
+import PizzaBuilder from "./PizzaBuilder";
 
 function Pizza({toppings, image, type}) {
 
+    const handleClick = (e) => {
+        console.log(type, image, toppings);
+    }
+
     return(
         <div className="menuNav">
-            <img src={image} />
-            <>{type}</>
-            <>{toppings}</>
+            <img src={image} alt={type} onClick={handleClick} style={{ "cursor":"pointer" }}/>
         </div>
     )
 
