3264bceb6fe835f0b481d2b3883ad41ad6e876d5
Elisabeth Aka <aea536@nyu.edu>
2023-02-13 13:02:06 -0500

    background

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import PizzaBuilder from './components/PizzaBuilder';
 import { useEffect, useState } from 'react';
+import background from "./backgroundPic.png";
 
 function App() {
 
@@ -13,7 +14,7 @@ function App() {
   },[])
 
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${background})`}}>
       <PizzaBuilder pizzas={pizzas} />
     </div>
   );
