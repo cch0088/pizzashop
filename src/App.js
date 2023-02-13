a0876e639a6e85802424fd5d4c395be174a2f5a7
Constantine Cherkashyn <cons1@protonmail.ch>
2023-02-12 20:40:59 -0500

    Basic layout done

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   },[])
 
   return (
-    <div>
+    <div className="App">
       <PizzaBuilder pizzas={pizzas} />
     </div>
   );
--- src/components/PizzaBuilder.js
+++ src/components/PizzaBuilder.js
@@ -1,8 +1,15 @@
 function PizzaBuilder ({pizzas}) {
 
     return (<div>
-        <div className="heading">Pizza Builder</div>
-        
+        <div className="heading">Heading</div>
+        <div className="navigation">Navigation Menu</div>
+        <div className="spacer">Blank space</div>
+        <div className="content">
+            <div className="picture">Picture</div>
+            <div className="options">Options</div>
+        </div>
+        <div className="lower-content">Lower Content</div>
+        <div className="footer">Footer</div>
     </div>);
 }
 
