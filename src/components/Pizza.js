e9b0521e44af07a5ed3f3addebbe7bee3090b098
Constantine Cherkashyn <cons1@protonmail.ch>
2023-02-15 18:36:47 -0500

    Pizza builder fix

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -9,8 +9,8 @@ function Pizza({toppings, image, type, setToppings, setType, setImage}) {
     }
 
     return(
-        <div className="menuNav">
-            <img src={image} alt={type} onClick={handleClick} style={{ "cursor":"pointer", "width":"400px", "height":"300px" }}/>
+        <div>
+            <img className="pizza-select" src={image} alt={type} onClick={handleClick} />
         </div>
     )
 
