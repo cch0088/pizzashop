40ca54b7f7789ab9a1d3a900f4f59239975c44e7
emilyphalperin@gmail.com <emilyhalperin@Emilys-MacBook-Pro-2.local>
2023-02-15 11:19:04 -0500

    CSS for orderlist

--- src/components/ListOrder.js
+++ src/components/ListOrder.js
@@ -3,10 +3,13 @@ import React from 'react';
 function ListOrder({type, toppings}) {
     return(
         <div className="order-content">
-            <ul>
-                <ol>Type: {type}</ol>
-                <ol>Toppings: {toppings}</ol>
+            <div className="center">
+            <ul className="div-box">
+                <ol className="order-info">Order Info</ol>
+                <ol className="center">Type: {type}</ol>
+                <ol className="center">Toppings: {toppings}</ol>
             </ul>
+            </div>
         </div>
     )
 }
