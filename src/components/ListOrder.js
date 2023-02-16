d546b3434cad40c3687d3aacd90049b07c9d6318
Constantine Cherkashyn <cons1@protonmail.ch>
2023-02-16 18:53:38 -0500

    Tidy Orders page

--- src/components/ListOrder.js
+++ src/components/ListOrder.js
@@ -4,10 +4,13 @@ function ListOrder({type, toppings, id}) {
     return(
         <div className="order-content">
             <div className="div-box">
-                <div className="order-info">Order Info</div>
-                <div className="center">Type: {type}</div>
-                <div className="center">Toppings: {toppings}</div>
-                <div className="center">Order number: {id}</div>
+                <div className="order-info">{type.charAt(0).toUpperCase() + type.slice(1)} Pizza</div>
+                <div className="center-left">
+                    <ul>{toppings.map((topping, index) => {
+                        return <li key={index}>{topping.charAt(0).toUpperCase() + topping.slice(1)}</li>})}
+                    </ul>
+                </div>
+                <div className="center">#{id}</div>
             </div>
         </div>
     )
