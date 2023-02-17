ed14987236827635fafb2ce6e1611bec569d63d0
Constantine Cherkashyn <cons1@protonmail.ch>
2023-02-17 09:11:45 -0500

    Fix contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,21 +4,16 @@ import React from 'react';
 function Contact() {
 
   return (
-    <div id ="contentList">
-      <div id="horz-list">
+    <div id ="horz-list">
         <ul>
-          <li>Phone Number: 555-555-5555</li>
-          <li>Email: info@pizzashop.com</li>
-          <li>Address: 11 Broadway New York, NY</li>
+          <li>Pizza Shop</li>
+          <li>11 Broadway</li>
+          <li>New York, NY 10012</li>
+          <li>555-555-5555</li>
+          <li>info@pizzashop.com</li>
         </ul>
-      </div>
     </div>
   );
 }
 
 export default Contact;
-
-/*
-11 Broadway,
-New York, NY 10012
-*/
