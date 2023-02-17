aa1976025e587ca8552f4860f44364f8209c8bba
Elisabeth Aka <aea536@nyu.edu>
2023-02-17 09:29:26 -0500

    homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { useEffect } from 'react';
-import PizzaDisplay from './PizzaDisplay';
 
-function Home ({}) {
+function Home () {
 
     return (
         <div className="homie">
             <h3>WELCOME TO OUR COOL PIZZA SHOP</h3>
+            <div className="description">
+                <p>Pizzashop is your new friend in town. We make your pizza ordoring hussle and headache free. Our website is designed to make the process of your online pizza ordering and delivery exiting . PIZZASHOP gives you a variety of options to customize your own pizza. Start your adventure now </p>
+            </div>
+        
         </div>
     )
 }
