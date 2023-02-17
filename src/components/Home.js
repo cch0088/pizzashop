8a1f5b383e460047b3c1798043b181a267c48edb
Constantine Cherkashyn <cons1@protonmail.ch>
2023-02-17 09:34:42 -0500

    Final

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,10 @@ function Home () {
         <div className="homie">
             <h3>WELCOME TO OUR COOL PIZZA SHOP</h3>
             <div className="description">
-                <p>Pizzashop is your new friend in town. We make your pizza ordoring hussle and headache free. Our website is designed to make the process of your online pizza ordering and delivery exiting . PIZZASHOP gives you a variety of options to customize your own pizza. Start your adventure now </p>
+                <p>Pizzashop is your new friend in town.
+                    We make your pizza ordoring hussle and headache free.
+                    Our website is designed to make the process of your online pizza ordering and delivery exciting.
+                    PIZZASHOP gives you a variety of options to customize your own pizza.</p>
             </div>
         
         </div>
